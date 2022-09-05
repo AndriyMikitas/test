2deb158084e2c1f13a13bbb9aeb6ecb039299336
AndriyMikitas <48184113+AndriyMikitas@users.noreply.github.com>
2022-09-05 11:43:52 +0300

    Add files via upload

---
+++ jdbctemplatetryout/src/main/java/com/example/jdbctemplatetryout/JdbctemplatetryoutApplication.java
@@ -0,0 +1,13 @@
+package com.example.jdbctemplatetryout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdbctemplatetryoutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JdbctemplatetryoutApplication.class, args);
+    }
+
+}
